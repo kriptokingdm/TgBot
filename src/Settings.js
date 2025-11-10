16311544597f6176de34643442c5b3d6a40501e5
kriptokingdm <kriptokingdm@gmail.com>
2025-11-10 06:20:46 +0300

    feat: add frontend files

---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Settings() {
+    return (
+        <div>
+            <h2>Настройки</h2>
+            {/* Логика настроек */}
+        </div>
+    );
+}
+
+// export default Settings;
+
